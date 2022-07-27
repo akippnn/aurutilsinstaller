49c1d225069cb2b1734f298e369d9a604a08ce96
Aki <80670461+akippnn@users.noreply.github.com>
2022-07-27 18:23:05 +0800

    Update aurutilsinstaller.sh
    - Fixed: The script failed when aurutils was not installed and git installed when it was already installed.
    - Echo commands when executed commands make changes to the system used (function from https://stackoverflow.com/a/23342259).
    - You can now add packages that were already built through the script. This is only very useful if you remember the path names of the packages, or you can use GUI file manager with drag and drop.

--- aurutilsinstaller.sh
+++ aurutilsinstaller.sh
@@ -40,7 +40,7 @@ MG='\033[0;35m' # Purple
 CY='\033[0;36m' # Cyan
 YL='\033[1;33m' # Yellow
 sh_repo_url="${CY}https://github.com/akippnn/aurutilsinstaller$NC"
-version="2"
+version="2.1"
 prefix="${YL}[aurutilsinstaller ver. $version]$NC"
 pkg="${MG}aurutils$NC"
 pkg_url="${CY}https://aur.archlinux.org/packages/aurutils$NC"
@@ -60,10 +60,12 @@ __aurremove="#!/bin/sh --
 	\npaccache -c \"\${repo_path%/*}\" -rvk0 \"\$@\"
 	\ndone"
 
+exe() { echo -e "${YL}\$${NC} ${@/eval/}" ; "$@" ; } # Function from https://stackoverflow.com/a/23342259
+
 __nonfresh="$prefix Warning: Do not use this script if you have a working aurutils install.
 	\nAs for now, this script is only meant to be deployed on fresh systems with no trace of $pkg.
 	\nPass the variable REQ_FRESH_INSTALL as false (case-insensitive) to ignore this warning.\n"
-	
+
 nonfresh () {
         if [ "$REQ_FRESH_INSTALL" = true ] ; then
                 # shellcheck disable=2086
@@ -78,7 +80,7 @@ if ! [ "$EUID" -ne 0 ] ; then
 	echo -e "$prefix Please do not run this script as root.\n"
 	exit
 ## Checks if aurutils is in pacman
-elif ! [[ $(pacman -Qi aurutils &> /dev/null) ]] ; then 
+elif [[ $(pacman -Qi aurutils &> /dev/null) ]] ; then 
 	nonfresh
 fi
 
@@ -102,42 +104,39 @@ done
 echo -e "\n$prefix Please answer the following:"
 read -rp 'Enter your new repository name [default: custompkgs]: ' REPO_NAME
 read -rp 'Where to place the repository [default: /home/custompkgs]: ' REPO_DIR
-REPO_NAME=${REPO_NAME:-custompkgs}
+REPO_NAME=${REPO_NAME:-custompkgs} # Would rather use parameter expansion instead of readline to input easily
 REPO_DIR=${REPO_DIR:-/home/custompkgs}
 
 echo -e "\nPlease pay attention to the terminal until the script is finished, it shouldn't take a while.\nAssuming you did not modify timestamp_timeout, password should mostly be asked once only."
-sudo install -d "$REPO_DIR" -o "$USER"
+exe sudo install -d "$REPO_DIR" -o "$USER"
 if ! [ -d "$REPO_DIR" ] ; then
 	echo -e "$prefix The repository directory the script was supposed to create does not exist. Please give a proper directory path."
 	exit
 fi
-repo-add "$REPO_DIR"/"$REPO_NAME".db.tar.gz
+exe repo-add "$REPO_DIR"/"$REPO_NAME".db.tar.gz
 
 
 ## INSTALL
 echo -e "$prefix Installing aurutils and its' dependencies"
-
 ## Git is a required to clone and is a dependency of aurutils (see pkg_url).
-if [[ $(pacman -Qi git 1> /dev/null) ]] ; then
-	sudo pacman -S git --asdeps --noconfirm
+if ! [[ $(pacman -Qi git 1> /dev/null) ]] ; then
+	exe sudo pacman -S git --asdeps --noconfirm
 fi
 
 # shellcheck disable=2164
-git clone $git_url && cd "$(basename "$_" .git)" || nonfresh || cd aurutils || exit
-if grep -Fx 'pkgname=aurutils' PKGBUILD 1> /dev/null ; then
-	makepkg -si --noconfirm
+exe git clone $git_url && cd "$(basename "$_" .git)" || nonfresh || cd aurutils || exit
+if [[ $(grep -Fx 'pkgname=aurutils' PKGBUILD) ]] ; then
+	exe makepkg -si --noconfirm
 else
 	echo -e "$prefix Not sure what is going on here. You may want to troubleshoot."
 	exit
 fi
 
+echo -e "$prefix Adding aurutils to repository"
 
-## FINALIZE
-echo -e "$prefix Setting up the repository and adding aurutils"
-
-mv aurutils*.pkg.tar* "$REPO_DIR"
-repo-add -n custom.db.tar.gz aurutils*.pkg.tar*
-sudo pacman -Syu
+exe mv aurutils*.pkg.tar* "$REPO_DIR"
+exe repo-add -n "$REPO_NAME".db.tar.gz aurutils*.pkg.tar*
+exe sudo pacman -Syu
 
 
 ## CONFIGURE
@@ -148,21 +147,23 @@ __pacmanconf="
 \nServer = file://$REPO_DIR"
 
 echo -e "\n$prefix Installation finished. What else would you like to do?"
-__options="1) Add repository $REPO_NAME to /etc/pacman.conf automatically
+__options="1) Add an existing built package in the repository (untested this version)
 \n2) Sync vifm package and set vicmd (required to view build files)
-\n3) Install aur-remove script (script from ${CY}man aur${NC})
-\n4) Setup CacheDir and CleanMethod automatically (broken: will not add $REPO_NAME repository in CacheDir)
-\n5) Quit"
+\n3) Add repository $REPO_NAME to /etc/pacman.conf automatically
+\n4) Install aur-remove script (script from ${CY}man aur${NC})
+\n5) Setup CacheDir and CleanMethod automatically (untested: may not add $REPO_NAME repository in CacheDir)
+\n6) Quit"
 
 while true ; do
         # shellcheck disable=2086
 	echo -e $__options
 	read -rp '#? ' input
 	case $input in
-		1 ) # shellcheck disable=2086
-		    echo -e $__pacmanconf | sudo tee -a /etc/pacman.conf;
-		    sudo pacman -Syu;
-		    echo "Done.";;
+		1 ) IFS=' ' read -rp "Drag and drop .pkg files (or type in their paths) and press enter\n> " ARRAY
+		    for i in "${ARRAY[@]}" ; do
+		        exe mv "$i" "$REPO_DIR"
+			exe repo-add -n "$REPO_NAME".db.tar.gz "$i"
+		    done
 		    
 		2 ) sudo pacman -S vifm; 
 		    read -rp "Set vicmd (leave blank to cancel): " VICMD;
@@ -171,27 +172,31 @@ while true ; do
 				    echo "$VICMD is not a command.";
 				    continue;
 			    fi;
-			    sudo sed -i "s/set vicmd=.*/set vicmd=$VICMD/" "${XDG_CONFIG_HOME:-~/.config}"/vifm/vifmrc;
+			    exe sudo sed -i "s/set vicmd=.*/set vicmd=$VICMD/" ${XDG_CONFIG_HOME:-~/.config}/vifm/vifmrc;
 		    fi;;
 
-		3 ) read -rp "Path to install the script [default: /usr/local/bin]" SCRIPT_DIR;
+		3 ) # shellcheck disable=2086
+		    exe echo -e $__pacmanconf | sudo tee -a /etc/pacman.conf;
+		    exe sudo pacman -Syu;
+		    echo "Done.";;
+		    
+		4 ) read -rp "Path to install the script [default: /usr/local/bin]" SCRIPT_DIR;
 		    SCRIPT_DIR=${SCRIPT_DIR:-/usr/local/bin};
 		    if [ -d "$SCRIPT_DIR" ] ; then
 		            # shellcheck disable=2086,2090
-			    echo -e $__aurremove | sudo tee "$SCRIPT_DIR"/aur-remove;
-			    chmod +x "$SCRIPT_DIR"/aur-remove;
+			    exe echo -e $__aurremove | sudo tee "$SCRIPT_DIR"/aur-remove;
+			    exe chmod +x "$SCRIPT_DIR"/aur-remove;
 			    echo "Done.";
 		    else
 			    echo "$SCRIPT_DIR is not a directory.";
 		    fi;;
 
-		4 ) sudo sed -i "/^#CacheDir/s/$/ \"$REPO_DIR\"/" /etc/pacman.conf;
-		    sudo sed -i "s/CleanMethod =.*/CleanMethod = KeepCurrent/" /etc/pacman.conf;
-		    sudo sed -i "/\(CacheDir\|CleanMethod\)/s/^#//g" /etc/pacman.conf;
+		5 ) exe sudo sed -i "/^#CacheDir/s/$/ \"$REPO_DIR\"/" /etc/pacman.conf;
+		    exe sudo sed -i "s/CleanMethod =.*/CleanMethod = KeepCurrent/" /etc/pacman.conf;
+		    exe sudo sed -i "/\(CacheDir\|CleanMethod\)/s/^#//g" /etc/pacman.conf;
 		    echo "Done.";;
 
-
-	    	5 ) echo -e "$prefix Goodbye <3.";
+	    	6 ) exe echo -e "$prefix Goodbye <3.";
 		    break;;
 	esac
 done
